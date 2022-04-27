3cc95c3c0eafc89d60f8ba8d13a04302d0d0c48d
Bakhtiyor Mulladzhanov <wizardino11@mail.ru>
2022-04-27 23:51:21 +0300

    lab8

---
+++ backend/front/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+    import NavigationBar from "./components/NavigationBarClass";
+    import Home from "./components/Home";
+    import Another_Home from "./components/Another_Home";
+
+    function App() {
+        return (
+            <div className="App">
+                {/* Смысл в том, что браузер привязывает компоненты к локальным путям внутри приложения */}
+                <BrowserRouter>
+                    <NavigationBar/>
+                    <div className="container-fluid">
+                        <Routes>
+                            <Route path="home" element={<Home />} />
+                            {/* Дополнение: создал новый компонент, чтобы разделить то, что выводится с каждой из ссылок */}
+                            <Route path="Another_Home" element={<Another_Home />} />
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+
+// Вот этой строчки в методе не было - пришлось добавлять её отдельно export default App;
+    export default App;
---
+++ backend/front/src/components/Another_Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Another_Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="mt-5 me-auto">
+                <h2>Лабораторная работа № 8 по РПО</h2>
+
+                <div className="mt-15 me-auto">
+                    <p>Мулладжанов Бахтиёр, студент группы ИУ3-61Б</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Another_Home;
